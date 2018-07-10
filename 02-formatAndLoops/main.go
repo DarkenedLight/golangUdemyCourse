c5cce765cbed198fd666b3403f76f8f3692dc2f7
DarkenedLight <justin97progamer@gmail.com>
2018-07-10 12:51:02 -0400

    added variables folder

--- 02-formatAndLoops/main.go
+++ 02-formatAndLoops/main.go
@@ -6,6 +6,10 @@ import (
 
 func main() {
 	for i := 0; i < 200; i++ {
+		//d: decimal
+		//b: binary
+		//x: hex
+		//q: ascii
 		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
 	}
 }
---
+++ 03-variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// shorthand - declare and assign using ':=', type auto set
+	a := 10
+	b := "golang"
+	c := true
+	// %T prints the type of the supplied variable
+	fmt.Printf("type: %T \n", a)
+	fmt.Printf("type: %T \n", b)
+	fmt.Printf("type: %T \n\n", c)
+
+	// var declaration - var *name* *type* = *value*
+	// can also omit "= *value*" to set it to the types "zero value"
+	// while you can declare the type while assigning, golang reccomends
+	// omitting type because it will be inferred
+	var d int = 2
+	var e = "hey"
+	var f bool
+	// %v prints the value, or zero value if none is assigned
+	fmt.Printf("value: %v \n", d)
+	fmt.Printf("value: %v \n", e)
+	fmt.Printf("value: %v \n", f)
+}
