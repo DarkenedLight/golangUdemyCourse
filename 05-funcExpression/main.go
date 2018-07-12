ff1e95bba53ae356516c35066461b86b1d320676
DarkenedLight <justin97progamer@gmail.com>
2018-07-12 14:34:45 -0400

    added another fun expression example

--- 05-funcExpression/main.go
+++ 05-funcExpression/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+//this is a func whose return type is a func that returns an int
+func wrapper() func() int {
+	var x = 0
+	return func() int {
+		x++
+		return x
+	}
+}
+
 func main() {
 	var x = 0
 
@@ -19,4 +28,13 @@ func main() {
 	// }
 
 	fmt.Println(foo())
+
+	//foo2 is assinged the anonymous fun that is defined by the return
+	//statemnt in func wrapper
+	var foo2 = wrapper()
+	fmt.Println(foo2())
+	fmt.Println(foo2())
 }
+
+//anonymous funcs are funcs without a name, like in a fun expression
+//func expressions are where you assign a fun to a variable
